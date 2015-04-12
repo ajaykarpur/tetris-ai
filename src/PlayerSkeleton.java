dd70a6f63b42990ffc26533022bffc12f5514a06
Fred Lafrance <lafrancef@gmail.com>
2015-04-12 17:54:25 +0800

    Added dryRunMove function to run a move on the copy of State.

--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -8,29 +8,17 @@ public class PlayerSkeleton {
 	 */
 	private class StateEx extends State {
 		
-		/**
-		 * Test a given move against the state.
-		 * @return The number of rows that would be cleared by
-		 * making the given move.
-		 */
-		public int[] getHeuristicVector() {
-			
-			int[] bumpinessAndHeight = getBumpinessAndHeight();
-			
-			return new int[] {this.getRowsCleared(),
-							  getHoles(),
-							  bumpinessAndHeight[0],
-							  bumpinessAndHeight[1]};
-		}
+		private int[][] fieldCopy;
+		private int[] topCopy;
 				
 		private int getHoles() {
 			int holes = 0;
 
 			//start looking for empty spaces below top of pile
-			for (int col = 0; col < this.getTop().length; col++){
-				for (int row = this.getTop()[col]; row >= 0; row--){
+			for (int col = 0; col < this.topCopy.length; col++){
+				for (int row = this.topCopy[col]; row >= 0; row--){
 					//if empty space encountered below top, it's a hole
-					if (this.getField[row][col] == 0)
+					if (this.fieldCopy[row][col] == 0)
 						holes++;
 				}
 			}
@@ -38,25 +26,96 @@ public class PlayerSkeleton {
 			return 0;
 		}
 		
-		private int[] getBumpinessAndHeight() {
-			int[] top = this.getTop();
+		private int[] getBumpinessAndHeight() {			
 			int bumpiness = 0;
-			int aggregateHeight = top[0];
-			for (int i = 1; i < top.length; i ++)
+			int aggregateHeight = topCopy[0];
+			for (int i = 1; i < topCopy.length; i ++)
 			{
-				bumpiness += Math.abs(top[i] - top[i-1]);
-				aggregateHeight += top[i];
+				bumpiness += Math.abs(topCopy[i] - topCopy[i-1]);
+				aggregateHeight += topCopy[i];
 			}
 			return new int[] {bumpiness, aggregateHeight};
 		}
 		
+		private float testMove(int orient, int slot) {
+			// TODO Copy data over to topCopy and fieldCopy
+			
+			int rowsCleared = dryRunMove(this.nextPiece, orient, slot);
+			int[] bumpinessAndHeight = getBumpinessAndHeight();
+			int[] heuristics = {this.getRowsCleared(),
+								getHoles(),
+								bumpinessAndHeight[0],
+								bumpinessAndHeight[1] };
+			
+			// TODO Compute score - heuristics dot weights.
+			
+			return 0.f;
+			
+		}
 		
-		
+		/**
+		 * Play the given move on our local copy of the board.
+		 * @return Number of rows cleared by the move.
+		 */
+		private int dryRunMove(int piece, int orient, int slot) {
+			//height if the first column makes contact
+			int height = topCopy[slot]-State.getpBottom()[piece][orient][0];
+			//for each column beyond the first in the piece
+			for(int c = 1; c < State.getpWidth()[piece][orient];c++) {
+				height = Math.max(height,topCopy[slot+c]-State.getpBottom()[piece][orient][c]);
+			}
+			
+			//for each column in the piece - fill in the appropriate blocks
+			for(int i = 0; i < State.getpWidth()[piece][orient]; i++)
+				for(int h = height+State.getpBottom()[piece][orient][i]; h < height+State.getpTop()[piece][orient][i]; h++)
+					fieldCopy[h][i+slot] = -1;
+			
+			//adjust top
+			for(int c = 0; c < State.getpWidth()[piece][orient]; c++) {
+				topCopy[slot+c]=height+State.getpTop()[piece][orient][c];
+			}
+			
+			//check if game ended
+			if(height+State.getpHeight()[piece][orient] >= ROWS)
+				return 0;
+			
+			int rowsCleared = 0;
+			
+			//check for full rows - starting at the top
+			for(int r = height+State.getpHeight()[piece][orient]-1; r >= height; r--) {
+				//check all columns in the row
+				boolean full = true;
+				for(int c = 0; c < COLS; c++) {
+					if(fieldCopy[r][c] == 0) {
+						full = false;
+						break;
+					}
+				}
+				//if the row was full - remove it and slide above stuff down
+				if(full) {
+					rowsCleared++;					
+					//for each column
+					for(int c = 0; c < COLS; c++) {
+
+						//slide down all bricks
+						for(int i = r; i < topCopy[c]; i++) {
+							fieldCopy[i][c] = fieldCopy[i+1][c];
+						}
+						//lower the top
+						topCopy[c]--;
+						while(topCopy[c]>=1 && fieldCopy[topCopy[c]-1][c]==0)	topCopy[c]--;
+					}
+				}
+				
+			}
+			
+			return rowsCleared;
+		}
 		
 	}
 	
 	//implement this function to have a working system
-	public int pickMove(State s, int[][] legalMoves) {
+	public int pickMove(StateEx s, int[][] legalMoves) {
 		
 		return 0;
 	}
